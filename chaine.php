25d7590b742102b0b449f24e954f04362b275b94
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-06-30 02:24:35 +0200

    string

---
+++ chaine.php
@@ -0,0 +1,9 @@
+<?php
+$nom = "feze";
+$prenom = "cedrick";
+$note1 = 10;
+$note2 = 20;
+$moyenne = ($note1 + $note2) / 2;
+echo "Bonjour " . $prenom . ' ' . $nom ." Vous avez eu " . $moyenne . " de moyenne\n";
+echo "Bonjour $prenom  $nom vous avez eu $moyenne de moyenne"; // l'interpolation
+
